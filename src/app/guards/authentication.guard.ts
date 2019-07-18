4f0a3d812c49933e0be96302b2d2a44f9d319cd5
Domonkos Papp <pappdomi5@gmail.com>
2019-07-18 09:05:42 +0200

    Implement auth guard

--- src/app/guards/authentication.guard.ts
+++ src/app/guards/authentication.guard.ts
@@ -1,10 +1,26 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, CanActivate, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthenticationService } from '../services/authentication.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthenticationGuard implements  {
-  
+export class AuthenticationGuard implements CanActivate {
+
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService
+  ) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+      const currentUsername = this.authenticationService.currentUsername;
+      if (currentUsername) {
+          // logged in so return true
+          return true;
+      }
+      // not logged in so redirect to login page with the return url
+      this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
+      return false;
+  }
 }
